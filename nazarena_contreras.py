1eeeb2cb471469010891e6d14dac992653c53680
Nazarena Contreras <nazarenacontreras9@gmail.com>
2025-01-26 18:13:29 -0300

    modificando

--- nazarena_contreras.py
+++ nazarena_contreras.py
@@ -1,5 +1,4 @@
 """
-Primer proyecto integrador python - nazarenacontreras.py
 ------------------- EL JUEGO SE LLAMA: ADIVINA LA PALABRA -------------------------              
 """
 import random #para elecciones aleatorias
@@ -17,13 +16,13 @@ def validar_letra(func):
 
 #clase JuegoAdivinaLaPalabra es una plantilla para crear objetos, agrupa datos (atributos) y funciones (métodos)
 class JuegoAdivinaLaPalabra:
-    #metodo constructor __init__ 
-    def __init__(self, palabra_secreta, intentos=6):          
-        self.palabra_secreta = palabra_secreta                
-        self.intentos = intentos                              
-        self.letras_adivinadas = []                           
-        self.estado_actual = ["_" for _ in palabra_secreta]   
-        self.letras_incorrectas = []                          
+    #metodo constructor __init__  metodo especial que se llama automáticamente cuando se crea un objeto.
+    def __init__(self, palabra_secreta, intentos=6):          #el parámetro intentos tiene un valor predeterminado de 6
+        self.palabra_secreta = palabra_secreta                #atributo que almacena la palabra que se debe adivinar
+        self.intentos = intentos                              #atributo que almacena el número de intentos que le quedan al jugador
+        self.letras_adivinadas = []                           #atrubuto como lista vacía donde se guardan las letras ingresadas adivinadas
+        self.estado_actual = ["_" for _ in palabra_secreta]   #atrubuto como lista de guines, donde cada guion representa una letra de la palabra
+        self.letras_incorrectas = []                          #atributo como lista vacia donde se guardan las letras ingresadas incorrectas
 
     @validar_letra        #el decorador se aplica a la función adivinar_letra para validar la letra antes de proceder con el resto de la lógica. 
     def adivinar_letra(self, letra):
@@ -37,15 +36,7 @@ class JuegoAdivinaLaPalabra:
             self.intentos -= 1 #para restar intentos cuando ingresa una letra incorrecta.
             self.letras_incorrectas.append(letra) #para agregar la letra incorrecta a la lista. 
             print("Letra incorrecta. Pierdes un intento.")
-        self.mostrar_estado() 
-        
-#para iniciar el juego
-juego = JuegoAdivinaLaPalabra(palabra_secreta)
-juego.jugar()
-
-#lista de palabras secretas
-palabras_secretas = ["camara", "auriculares", "celular", "monitor", "teclado"]
-palabra_secreta = random.choice(palabras_secretas)
+        self.mostrar_estado()
 
     #para mostrar el estado de letras adivinadas, intentos y letras incorrectas.
     def mostrar_estado(self):
@@ -57,16 +48,23 @@ palabra_secreta = random.choice(palabras_secretas)
     
     #metodo jugar que controla el flujo del juego
     def jugar(self):
-        print("¡Bienvenida al juego Adivina la Palabra!  Para iniciar ingresa una letra y si deseas salir escribe 'salir' en cualquier momento.")
+        print("¡Bienvenida al juego Adivina la Palabra! \U0001F604 Para iniciar ingresa una letra y si deseas salir escribe 'salir' en cualquier momento.")
         self.mostrar_estado()
         while self.intentos > 0 and not self.verificar_victoria(): #bucle while para continuar ejecutando mientras queden intentos.
             letra = input("Adivina una letra: ").lower()
             if letra == "salir":
-                print("Decidiste no jugar mas. Fin del juego.")
+                print("Decidiste no jugar más \U0001F615 \nFin del juego.")
                 return
             self.adivinar_letra(letra)
         if self.verificar_victoria():
             print("¡Felicidades! ¡Adivinaste la palabra!")
         else:
-            print(f"Perdiste. La palabra secreta era '{self.palabra_secreta}'. Fin del juego.")
-        print("gracias por jugar")
+            print(f"Lo siento \U0001F641 perdiste. La palabra secreta era '{self.palabra_secreta}'. \nFin del juego.")
+
+#lista de palabras secretas
+palabras_secretas = ["camara", "auriculares", "celular", "monitor", "teclado"]
+palabra_secreta = random.choice(palabras_secretas)
+
+#iniciar el juego
+juego = JuegoAdivinaLaPalabra(palabra_secreta)
+juego.jugar()
